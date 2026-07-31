ca88a9b7fcca1bd11123a674a4690e124d0fb554
yomogimochi139 <ryouhei6713@icloud.com>
2026-07-31 23:58:09 +0900

    完成?

--- 0731kadai/0731kadai/0731kadai.cpp
+++ 0731kadai/0731kadai/0731kadai.cpp
@@ -1,7 +1,56 @@
 ﻿#include <iostream>
+#include<memory>
+#include "character.h"
+#include "Manager.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	int playerHp = 300;
+	int enemyHp = 300;
+	int turn = 1;
 
+	int playerCharachoice = 0;
+
+	while (playerHp > 0 && enemyHp > 0) {
+		std::cout << "ターン " << turn << std::endl;
+		std::cout << "現在のプレイヤー戦力" << playerHp << std::endl;
+		std::cout << "現在の敵戦力" << enemyHp << std::endl;
+
+		std::cout << "使用キャラクターを選択してください (1: 剣士, 2: 魔法使い, 3: 召喚士): ";
+
+		std::cin >> playerCharachoice;
+	}
+
+	std::cout << "行動を選択してください (1: 攻撃, 2: 防御): " << std::endl;
+
+	int playerAction = 0;
+	while (playerAction != 1 && playerAction != 2) {
+		std::cin >> playerAction;
+	}
+
+	std::unique_ptr<CharaBase> playerCharacter = createCharacter(playerCharachoice, 0);
+	std::unique_ptr<CharaBase> enemyCharacter = createCharacter(getRandomInt(1, 3), 1);
+
+	int enemyAction = getRandomInt(1, 2);
+
+	evaluatePhase(*playerCharacter, *enemyCharacter, playerAction, enemyAction);
+	playerHp = playerCharacter->param().hp;
+	enemyHp = enemyCharacter->param().hp;
+
+	std::cout << "プレイヤーの残り戦力: " << playerHp << std::endl;
+	std::cout << "敵の残り戦力: " << enemyHp << std::endl;
+
+	turn++;
+    
+	if (playerHp > 0&&enemyHp <= 0) {
+		std::cout << "勝利！" << std::endl;
+	}
+	else if (enemyHp > 0&&playerHp <= 0) {
+		std::cout << "敗北" << std::endl;
+	}
+	else
+	{
+		std::cout << "引き分け" << std::endl;
+	}
+	return 0;
+}
