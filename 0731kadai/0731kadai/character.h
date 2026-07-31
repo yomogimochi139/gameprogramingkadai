455db55352fedb25523be68c5a7f271066e068e0
yomogimochi139 <ryouhei6713@icloud.com>
2026-07-31 13:53:37 +0900

    途中

---
+++ 0731kadai/0731kadai/character.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <memory>
+// キャラクターのパラメータ
+struct charaParam
+{
+int hp{}; //戦力
+int attack{}; //攻撃力
+};
+
+class CharaBase {
+public:
+	//コンストラクタ
+	CharaBase();
+
+	//デストラクタ
+	virtual ~CharaBase();
+
+	//攻撃
+	int Attack();
+
+	//ダメージ
+	void damage(int damageSourse);
+
+	//戦力が0か
+	bool deaded() const;
+
+	//キャラ名取得
+	const char* name() const;
+
+	//キャラのパラメータを取得
+	const charaParam& param() const;
+
+	//敵か味方か
+	int faction() const;
+
+	//キャラのID取得
+	int charaId() const;
+
+public:
+	//キャラの種類の初期化
+	virtual void initializeType() = 0;
+
+
+	virtual void WhenupdateStartTurn() = 0;
+
+protected:
+	charaParam param_{};  //キャラ毎のパラメータ
+	char       name_[64]; //キャラの名前
+	int        charaId_{};//キャラId
+	int        faction_{};//敵か味方
+};
---
+++ 0731kadai/0731kadai/judge.h
@@ -0,0 +1,12 @@
+#pragma once
+class judge
+{
+public:
+    
+
+    enum class Action {
+        Attack, //UŒ‚
+        Difense //–hŒä
+    };
+};
+
